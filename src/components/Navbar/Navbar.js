52bf3889d0561dd6f3cdcaae896ef3b8b6e933b5
rabeyaalfaz <rabeyaalfaz3@gmail.com>
2024-02-12 13:59:51 +0600

    finally it's done

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -9,7 +9,7 @@ export default function Navbar() {
           <Sidebar></Sidebar>
             <motion.span initial={{opacity:0,scale:0.5}} animate={{opacity:1,scale:0.5}} transition={{duration:0.5}}><a href='https://drive.google.com/file/d/155QUhWNFFaV9hPmEEDkAI5d37BpO3QgN/view?usp=sharing'>Rabeya Alfaz(Resume)</a></motion.span>
             <div className="social-icon">
-                <a href="https://www.linkedin.com/in/rabeya-alfaz-2b39a3270/"><img src="/linkedIn.png" alt="" /></a>
+                <a href="https://www.linkedin.com/in/rabeya-alfaz-2b39a3270/"><img src="/linkedIn.jpg" alt="" /></a>
                 <a href="https://github.com/RabeyaAlfaz"><img src="/github.jpg" alt="" /></a>
                 <a href="#"><img src="/facebook.png" alt="" /></a>
             </div>
